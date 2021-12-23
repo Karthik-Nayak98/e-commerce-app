a02cffff5729e8565f7767ef0218fe407c1bc6f4
karthik nayak <karthik.nayak@oracle.com>
2021-12-23 23:10:30 +0530

    feat: add responsive css to navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {FaUserCircle} from 'react-icons/fa'
+import {IoMdClose} from 'react-icons/io'
 
 import { BsBagCheck } from 'react-icons/bs';
 import { CartContext } from '../../context/cartContext';
@@ -10,9 +13,29 @@ import Logo from '../../assets/shoppy.png';
 import './navbar.styles.css';
 
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+  const [width, setWidth] = useState(0);
+
   const { totalItems } = useContext(CartContext);
   const user = useContext(AuthContext);
   const navigate = useNavigate();
+
+  useEffect(()=> {
+    const handleRezize = ()=>{
+      setWidth(window.innerWidth)
+      if(width > 600)
+        setToggle(false)
+    }
+    window.addEventListener('resize',handleRezize)
+    return ()=> {
+      window.removeEventListener('resize', handleRezize)
+    }
+  },[width])
+
+
+  const toggleNavbar = () => {
+    setToggle(!toggle)
+  };
 
   const handleClick = () => {
     userSignOut();
@@ -24,7 +47,12 @@ function Navbar() {
       <figure>
         <img className='logo' src={Logo} alt='' />
       </figure>
-      <ul className='navitems'>
+      <div className='hamburger' onClick={toggleNavbar}>
+       {!toggle ? <GiHamburgerMenu color='#222' size='1.5rem'/>:
+       <IoMdClose color='#222' size='1.5rem'/>}
+       
+      </div>
+       <ul className={toggle? 'navitems': 'navitems active'}>
         <li>
           <Link style={{ textDecoration: 'none' }} to='/'>
             Home
@@ -45,12 +73,16 @@ function Navbar() {
           )}
         </li>
         <li>
-          <Link to='/cart'>
+          <Link to='/cart' style={{textDecoration:'none'}}>
+            Cart {' '}
             <BsBagCheck className='navicon' />
             {totalItems ? <span className='cartlength'>{totalItems}</span> : ''}
           </Link>
         </li>
-        {user ? <li className='username'> {user.displayName[0]} </li> : null}
+        {!user ? <li className='username'> 
+       <FaUserCircle className='user'/>
+        {/* {user.displayName[0]}  */}
+        </li> : null}
       </ul>
     </nav>
   );
