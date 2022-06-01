d983748e4387236ce8d7132174a0283343ca5dec
karthik <karunayak63@gmail.com>
2022-06-01 21:29:24 +0530

    feat: add signin as guest user

--- src/pages/signin/signinpage.jsx
+++ src/pages/signin/signinpage.jsx
@@ -1,54 +1,71 @@
-import React, { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
-import Header from '../../components/header/header.component';
-import Button from '../../components/button/button.component';
-import InputContainer from '../../components/input-container/input-container';
-import FormInput from '../../components/form-input/form-input.component';
-import FormLabel from '../../components/form-label/form-label.component';
-import Spinner from '../../components/spinners/spinner';
+import Header from '../../components/header/header.component'
+import Button from '../../components/button/button.component'
+import InputContainer from '../../components/input-container/input-container'
+import FormInput from '../../components/form-input/form-input.component'
+import FormLabel from '../../components/form-label/form-label.component'
+import Spinner from '../../components/spinners/spinner'
 
-import { signInWithEmail } from '../../firebase/firebase-auth';
+import { signInWithEmail } from '../../firebase/firebase-auth'
 
-import './signin.styles.css';
+import './signin.styles.css'
 
 const initialState = {
   email: '',
   password: '',
-};
+}
 
 const SignIn = () => {
   const [isLoading, setIsLoading] = useState(false)
-  const [form, setForm] = useState(initialState);
+  const [form, setForm] = useState(initialState)
   const [error, setError] = useState('')
 
-  const navigate = useNavigate();
-  const { state } = useLocation();
-  const path = state ? state.path : null;
+  const navigate = useNavigate()
+  const { state } = useLocation()
+  const path = state ? state.path : null
 
   function handleChange(event) {
-    const key = event.target.name;
-    const inputValue = event.target.value;
+    const key = event.target.name
+    const inputValue = event.target.value
 
-    setForm({ ...form, [key]: inputValue });
+    setForm({ ...form, [key]: inputValue })
     setError('')
   }
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmitGuest = async (event) => {
+    event.preventDefault()
     setIsLoading(true)
+    const email = 'guest@gmail.com'
+    const password = 'guest@123'
+
     try {
-      await signInWithEmail(form.email, form.password);
+      await signInWithEmail(email, password)
       setIsLoading(false)
-      navigate(path || '/');
+      navigate(path || '/')
     } catch (err) {
-      if(err.code === 'auth/wrong-password')
+      if (err.code === 'auth/wrong-password')
         setError('Please enter the correct password.')
-      else if(err.code === 'auth/user-not-found')
-        setError('User is not found.')
+      else if (err.code === 'auth/user-not-found') setError('User is not found.')
       setIsLoading(false)
     }
-  };
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setIsLoading(true)
+    try {
+      await signInWithEmail(form.email, form.password)
+      setIsLoading(false)
+      navigate(path || '/')
+    } catch (err) {
+      if (err.code === 'auth/wrong-password')
+        setError('Please enter the correct password.')
+      else if (err.code === 'auth/user-not-found') setError('User is not found.')
+      setIsLoading(false)
+    }
+  }
 
   return (
     <div className='form__container'>
@@ -81,18 +98,25 @@ const SignIn = () => {
         <Link to='/signup'>
           <p className='signup'>Do not have an account? Register here!!</p>
         </Link>
-        {isLoading? <Spinner/>:null}
+        {isLoading ? <Spinner /> : null}
         <div className='button__container'>
+          <Button
+            type='button'
+            onClick={handleSubmitGuest}
+            title='Sign in as guest'
+            classname={`btn-signup ${isLoading ? 'disable' : ''}`}
+            disabled={isLoading}
+          />
           <Button
             type='submit'
             title='Sign in'
-            classname={`btn-signup ${isLoading? 'disable':''}`}
+            classname={`btn-signup ${isLoading ? 'disable' : ''}`}
             disabled={isLoading}
           />
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
