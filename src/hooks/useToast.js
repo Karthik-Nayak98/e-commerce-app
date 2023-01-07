d730b41bca2a90a14d829233d3a3064eb3f2a3e9
karthik <karunayak63@gmail.com>
2023-01-07 11:15:22 +0530

    add custom hooks for toast message

---
+++ src/hooks/useToast.js
@@ -0,0 +1,55 @@
+import { toast } from 'react-toastify';
+
+export default function useToast() {
+  const successToast = (text) => {
+    toast.success(text, {
+      position: 'top-right',
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const errorToast = (text) => {
+    toast.error(text, {
+      position: 'top-right',
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const warningToast = (text) => {
+    toast.warn(text, {
+      position: 'top-right',
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+  const infoToast = (text) => {
+    toast.info(text, {
+      position: 'top-right',
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return {
+    successToast,
+    errorToast,
+    warningToast,
+    infoToast,
+  };
+}
