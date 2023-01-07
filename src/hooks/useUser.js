371f09f29f41eb3296069040395a9ae44ae2ccfe
karthik <karunayak63@gmail.com>
2023-01-07 11:27:18 +0530

    display profile details from firebase

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../firebase/firebase.utils';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { auth } from '../firebase/firebase.utils';
 import { setUser } from '../redux/slice/userSlice';
 
 const useUser = () => {
@@ -10,6 +10,7 @@ const useUser = () => {
     const unsubscribe = onAuthStateChanged(
       auth,
       (user) => {
+        console.log(user);
         user ? dispatch(setUser(user.uid)) : dispatch(setUser(undefined));
       },
       []
--- src/redux/slice/userSlice.js
+++ src/redux/slice/userSlice.js
@@ -2,13 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const userSlice = createSlice({
   name: 'userslice',
-  initialState: { uid: null },
+  initialState: { uid: null, show: false },
   reducers: {
     setUser: (state, action) => {
       state.uid = action.payload;
     },
+
+    setShow: (state, action) => {
+      state.show = action.payload;
+    },
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setShow, setUser } = userSlice.actions;
 export default userSlice.reducer;
