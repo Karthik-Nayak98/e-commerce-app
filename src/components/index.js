3a3aa0baa6a5bfed41f50a605a12bbb60ba065e2
karthik <karunayak63@gmail.com>
2023-01-07 12:47:44 +0530

    add index file to import components in a neat way

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Button from './Button/Button';
+import Checkout from './Checkout/Checkout';
+import Container from './Container/Container';
+import EmptyCart from './EmptyCart/EmptyCart';
+import FormInput from './FormInput/FormInput';
+import FormLabel from './FormLabel/FormLabel';
+import Header from './Header/Header';
+import Icon from './Icon/Icon';
+import InputContainer from './InputContainer/InputContainer';
+import MenuItem from './MenuItem/MenuItem';
+import Navbar from './Navbar/Navbar';
+import Product from './Product/Product';
+import Spinner from './Spinner/Spinner';
+import StripeCheckoutButton from './StripeCheckoutButton/StripeCheckoutButton';
+import UserDetails from './UserDetails/UserDetails';
+
+export {
+  Button,
+  Checkout,
+  Container,
+  EmptyCart,
+  FormInput,
+  FormLabel,
+  Header,
+  Icon,
+  InputContainer,
+  MenuItem,
+  Navbar,
+  Product,
+  Spinner,
+  StripeCheckoutButton,
+  UserDetails,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Cart from './Cart/Cart';
+import Description from './Description/Description';
+import Error from './Error/Error';
+import Home from './Home/Home';
+import Login from './Login/Login';
+import Products from './Products/Products';
+import Protected from './Routes/Protected';
+import SignUp from './SignUp/SignUp';
+import Wishlist from './Wishlist/Wishlist';
+
+export {
+  Cart,
+  Wishlist,
+  Login,
+  SignUp,
+  Error,
+  Protected,
+  Products,
+  Home,
+  Description,
+};
