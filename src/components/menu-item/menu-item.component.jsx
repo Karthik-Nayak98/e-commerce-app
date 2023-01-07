6acf12a53934295d64f8605e118c5bae150574f2
karthik <karunayak63@gmail.com>
2023-01-07 11:57:15 +0530

    update category url for fetching category data

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,11 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 import './menu-item.styles.css';
-function MenuItem({ title, imageUrl, url }) {
+function MenuItem({ title, imageUrl, category }) {
   return (
     <div style={{ backgroundImage: `url(${imageUrl})` }} className='menu-item'>
-      <Link to={`/${url}`} className='link'>
+      <Link to={`category/${category}`} className='link'>
         <div className='content'>
           <h2 className='title'>{title}</h2>
           <span className='subtitle'>Shop Now</span>
@@ -20,5 +20,5 @@ export default MenuItem;
 MenuItem.propTypes = {
   title: PropTypes.string.isRequired,
   imageUrl: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-}
+  category: PropTypes.string.isRequired,
+};
